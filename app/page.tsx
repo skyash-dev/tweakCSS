656cbd45706183cfeecaaa495560688edc8f380b
CHAMP010 <yashpc09@gmail.com>
2024-03-25 21:18:14 +0530

    palette-done

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,30 @@ import { WavyBackground } from "@/components/ui/wavy-background";
 import Navbar from "@/components/navbar/navbar";
 
 export default function Home() {
+  const palettes = [
+    [
+    "cdb4db",
+    "ffc8dd",
+    "ffafcc",
+    "bde0fe",
+    "a2d2ff"
+    ],
+    [
+      "0fa3b1",
+      "b5e2fa",
+      "f9f7f3",
+      "eddea4",
+      "f7a072"
+    ],
+    ["03045e",
+    "023e8a",
+    "0077b6",
+    "0096c7",
+    "48cae4",
+    "90e0ef",
+    "ade8f4",
+    "caf0f8"]
+  ]
 
   return (
     <main className="">
@@ -15,13 +39,20 @@ export default function Home() {
           <div className="w-[60%]">
           <span className="text-3xl font-semibold">Popular Palettes</span>
         <div className="flex flex-col items-center my-6">
-        <div className="palette bg-red-500 w-11/12 h-56 rounded-xl flex">
-          <div className="w-1/5 hover:w-2/5 transition-all h-full bg-green-500 rounded-l-xl"></div>
-          <div className="w-1/5 hover:w-2/5 transition-all h-full bg-yellow-500 "></div>
-          <div className="w-1/5 hover:w-2/5 transition-all h-full bg-purple-500 "></div>
-          <div className="w-1/5 hover:w-2/5 transition-all h-full bg-slate-500 "></div>
-          <div className="w-1/5 hover:w-2/5 transition-all h-full bg-amber-500 rounded-r-xl"></div>
+          {palettes.map((palette)=>{
+            return(
+        <div className="palette w-11/12 h-56 flex my-6">
+          {palette.map((color, index)=>{
+            return(
+              <div className={`w-1/5 hover:w-2/5 transition-all h-full flex justify-center items-center text-[0] hover:text-base text-black cursor-pointer`} style={{backgroundColor: `#${color}`}}>
+                {color}
+              </div>
+
+            )
+          })}
         </div>
+            )
+          })}
         </div>
           </div>
       </div>
@@ -40,8 +71,8 @@ function Hero(){
       </p>
       <div className="flex justify-center">
       <div className="flex justify-between w-[45%]">
-      <button className="text-white bg-blue-500 py-2 px-3 rounded-md border-[1px] my-4 border-black bg-opacity-50 hover:bg-opacity-100 transition-all">Getting Started</button>
-      <button className="text-white bg-black bg-opacity-50 py-2 px-3 rounded-md border-[1px] my-4 border-black hover:bg-opacity-100 transition-all">Naming Conventions</button>
+      <button className="text-white bg-blue-500 py-2 px-3 rounded-md border-[1px] my-4 border-black bg-opacity-50 hover:bg-opacity-100 transition-all border-opacity-20">Getting Started</button>
+      <button className="text-white bg-black bg-opacity-50 py-2 px-3 rounded-md border-[1px] my-4 border-black hover:bg-opacity-100 transition-all border-opacity-20">Naming Conventions</button>
       </div>
       </div>
     </div>  

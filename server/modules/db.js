2cebbbdd6c19a99deb37be235cca36abdd194016
Yash <52105266+skyash101@users.noreply.github.com>
2024-04-08 12:03:21 +0530

    Update db.js

--- server/modules/db.js
+++ server/modules/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = function connectToMongo() {
     mongoose.connect(process.env.MONGO_URI)
     .then( () => {
-        console.log("DB CONNECTION SUCESSFUL!")
+        console.log("DB CONNECTION SUCCESSFUL!")
     })
     .catch( (e) => {
         console.log(e);
